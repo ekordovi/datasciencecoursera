ef77c2e950df90e56fbba21b5b93a01129d5f33d
Evan_Kordovi <ekordovi@gmail.com>
2024-02-28 14:46:49 -0800

    commit for the change in file name from Air_Poll_1.R to pollutntmean.R

---
+++ pollutantmean.R
@@ -0,0 +1,9 @@
+setwd("~/Desktop/R_files/datasciencecoursera")
+
+# trying to calc mean from a bunch of pollution sensors 
+pullutantmean <- function(directory, pollutant, id = 332) {
+  for (i in id){
+    read.csv("001.csv")
+  }
+  
+}
