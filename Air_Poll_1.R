f4bcf5a5d40651828995a94e35b0c635f73e99f5
Evan_Kordovi <ekordovi@gmail.com>
2024-02-28 14:50:12 -0800

    deletion of Air_Poll_1.R

--- Air_Poll_1.R
+++
@@ -1,9 +0,0 @@
-setwd("~/Desktop/R_files/datasciencecoursera")
-
-# trying to calc mean from a bunch of pollution sensors 
-pullutantmean <- function(directory, pollutant, id = 332) {
-  for (i in id){
-    read.csv("001.csv")
-  }
-  
-}
